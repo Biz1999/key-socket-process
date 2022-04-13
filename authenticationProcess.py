95eae9dd4da5ef2791e3669a45d5b4c00f29b080
ALESSANDRO BIZ <unifabiz>
2022-04-12 22:22:54 -0300

    server side socket commented

--- authenticationProcess.py
+++ authenticationProcess.py
@@ -22,3 +22,24 @@ def validateData(data):
     return True
 
 main()
+
+
+# import socket               # Import socket module
+# s = socket.socket()         # Create a socket object
+# Host = '0.0.0.0' # Get local machine name
+# port = 12345                # Reserve a port for your service.
+
+
+# print ('Server started!')
+# print ('Waiting for clients...')
+
+# s.bind((Host, port))        # Bind to the port
+# s.listen(5)                 # Now wait for client connection.
+# c, addr = s.accept()     # Establish connection with client.
+# print('Got connection from', addr)
+# while True:
+#   msg = c.recv(1024)
+#   print(addr, ' >> ', msg.decode("utf-8"))
+#   msg = input('SERVER >> ').encode("utf-8")
+#   c.send(msg)
+#   #c.close()                # Close the connection
